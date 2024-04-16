babf21d28adedf5b83eb376deff615ad1cd8bb8a
Mohammed Adel Mohammed <107713170+MoAdelEzz@users.noreply.github.com>
2024-04-17 01:23:59 +0200

    Transform implemented and tested

--- source/common/ecs/transform.cpp
+++ source/common/ecs/transform.cpp
@@ -3,6 +3,15 @@
 
 #include <glm/gtx/euler_angles.hpp>
 
+// TODO: remove this
+#include <iostream>
+
+#define xAxis glm::vec3(1.,0.,0.)
+#define yAxis glm::vec3(0.,1.,0.)
+#define zAxis glm::vec3(0.,0.,1.)
+
+using std::cout;
+
 namespace our {
 
     // This function computes and returns a matrix that represents this transform
@@ -10,7 +19,18 @@ namespace our {
     // HINT: to convert euler angles to a rotation matrix, you can use glm::yawPitchRoll
     glm::mat4 Transform::toMat4() const {
         //TODO: (Req 3) Write this function
-        return glm::mat4(1.0f); 
+        
+        glm::mat4 identityMatrix = glm::mat4(1.0);
+
+        glm::mat4 scalingMatrix = glm::scale(identityMatrix, this->scale);
+
+        glm::mat4 rotationMatrix = glm::yawPitchRoll(rotation.y, rotation.x, rotation.z);
+    
+        glm::mat4 translationMatrix = glm::translate(identityMatrix, this->position);
+    
+        glm::mat4 answer = translationMatrix * rotationMatrix * scalingMatrix;
+
+        return answer; 
     }
 
      // Deserializes the entity data and components from a json object

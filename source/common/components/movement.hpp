2929a67753cbd967de6a8628eea86e002812fa51
Kareem Alaa Abdullah <96543783+KareemAlaa1@users.noreply.github.com>
2024-05-05 07:59:43 +0300

    solving error

--- source/common/components/movement.hpp
+++ source/common/components/movement.hpp
@@ -69,11 +69,6 @@ namespace our
             return M * vec4(forward, 0.0f);
         }
 
-        void setCurrentPositionInWorld(glm::vec3 position)
-        {
-            getOwner()->localTransform.position = position;
-        }
-
         glm::vec3 getLookAtPoint(glm::mat4 M)
         {
             return M * vec4(forward, 1.0f);
